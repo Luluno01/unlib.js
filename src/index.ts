795e57c32f9d5bb005e7f5aec39a97d4b304cb98
Untitled <littleluluno01@163.com>
2019-12-17 23:43:14 +0800

    fix index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-/// <reference path="./Obj.d.ts" />
 import * as _Generators from './Generators'
 import * as _Random from './Random'
 import * as _Constants from './Constants'
@@ -7,7 +6,7 @@ import * as _Time from './Time'
 import * as _Prom from './Prom'
 import * as _fs from './fs'
 import * as _JSON from './JSON'
-import Obj from './Obj'
+import Obj, { EnhancedObjectConstructor } from './Obj'
 import * as _Math from './Math'
 import _TaskQueue from './TaskQueue'
 import * as _Sync from './Sync'
